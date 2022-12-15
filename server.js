9b881daa743c65a14f808703f7a3f1a593b4d8f5
Juniper James <vvlpespro@gmail.com>
2022-12-15 01:01:35 -0500

    it almost works

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const PORT = 3000;
 
 const methodOverride = require("method-override");
-
+const bodyParser = require("body-parser");
 const express = require("express");
 const pokemon = require("./models/pokemon.js");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
 app.use(methodOverride("_method"));
@@ -18,46 +24,58 @@ app.get("/pokemon", (req, res) => {
   });
 });
 
-//Show
+//New
+
+app.get("/pokemon/new", (req, res) => {
+  res.render("new.ejs",{
+    id: pokemon.length+1
+  });
+});
+
+//Destroy
+app.delete("/pokemon/:id", (req, res) => {
+  pokemon.splice(req.params.id, 1);
+  res.redirect("/pokemon");
+});
+
+//update
+
+app.get("/pokemon/:id/edit", (req, res) => {
+  res.render("edit.ejs", {
+    id: pokemon[req.params.id],
+    index: req.params.id
+  });
+});
+
+///Create
+
+app.post("/pokemon/", (req, res) => {
+    console.log(req.body)
+   
+    pokemon.push(req.body)
+    res.redirect('/pokemon')
+    
+});
+
+//edit
+app.put("/pokemon/:id", (req, res) => {
+  console.log(req.body);
+  pokemon[req.params.id].type = req.body.type;
+  pokemon[req.params.id].name = req.body.name;
+  pokemon[req.params.id].misc.classification = req.body.classification;
+
+  res.redirect("/pokemon");
+});
+
+//show
 
 app.get("/pokemon/:id", (req, res) => {
   res.render("show.ejs", {
     id: pokemon[req.params.id],
   });
 });
-//New
-
-app.get("/pokemon/new", (req, res) => {
-  res.render("new.ejs");
-});
-
-//Edit
-
-app.get("/pokemon/:id/edit", (req, res) => {
-  res.render("edit.ejs", {
-    id: pokemon[req.params.id],
-  });
-});
-
-///Create
-
-app.post("/pokemon", (req, res) => {
-  console.log("new");
-});
-
-//Update
-app.put("/pokemon/:id", (req, res) => {
-  
-  pokemon[req.params.id] = req.body;
-});
-
-//Destroy
-app.delete("/pokemon/:id", (req, res) => {
-  pokemon.splice(req.params.id, 1);
-  res.redirect('/pokemon')
-  });
-
 
 app.listen(PORT, () => {
   console.log("port " + PORT + " I choose you!");
 });
+ 
